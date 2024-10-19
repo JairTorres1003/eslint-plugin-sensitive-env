4df1c93cf97c4aa21050933e0c1b22d815f9d86f
JairTorres1003 <jairalex0306.jatc@gmail.com>
2024-10-19 00:55:36 -0500

    Refactor no-hardcoded-values rule and configuration

--- src/rules/no-hardcoded-values.ts
+++ src/rules/no-hardcoded-values.ts
@@ -1,10 +1,32 @@
 import * as fs from 'fs'
 
+import { type TSESLint } from '@typescript-eslint/utils'
 import * as dotenv from 'dotenv'
 
 import { IDENTIFIERS } from '@/constants/misc'
-import { type NoHardcodedValuesRule } from '@/interfaces/no-hardcoded-values'
 import { filterValuesByIdentifiers, getEnvironmentFile } from '@/utils/misc'
+
+export interface NoHardcodedValuesRuleOptions {
+  /**
+   * The path to the environment file.
+   * @example '.env.local'
+   */
+  envFile?: string
+  /**
+   * The identifiers to check for hardcoded values in the environment file.
+   * @default ['API_KEY', 'URL', 'TOKEN', 'PASSWORD', 'SECRET', 'UUID', 'KEY', 'DOMAIN']
+   */
+  identifiers?: Array<Uppercase<string>>
+}
+
+export type NoHardcodedValuesRuleMessageIds =
+  | 'noHardcodedValuesRule'
+  | 'environmentFileDoesNotExist'
+
+export type NoHardcodedValuesRule = TSESLint.RuleModule<
+  NoHardcodedValuesRuleMessageIds,
+  NoHardcodedValuesRuleOptions[]
+>
 
 const noHardcodedValuesRule: NoHardcodedValuesRule = {
   meta: {
