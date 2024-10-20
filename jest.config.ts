5ac67532ce9e8441f4a62097ee8b28b32a917f4a
JairTorres1003 <jairalex0306.jatc@gmail.com>
2024-10-19 21:19:47 -0500

    Refactor .eslintignore and .vscode/launch.json files

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/test/**/*.spec.ts'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+export default config
